48743e595e9ef9970d3e1f29f08f353c8b549eb7
AnthonyScott <anthony.scott1005@gmail.com>
2024-11-27 23:26:50 -0500

    Add script for environment setup

---
+++ setupPI.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+setup_env() {
+    sudo apt update
+    sudo apt install python3-dev python3-setuptools python3-pip gcc make
+
+    source SHH/bin/activate
+
+    pip install testresources
+    pip install lgpio --only-binary :all:
+    pip install Adafruit-Blinka
+    pip install -r requirements.txt
+}
+
+setup_env
